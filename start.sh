4313a51945bbd5cc12da702d24672719e282eaa1
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-02-17 01:29:44 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 export TERM=xterm
 
-if [ -z "`/etc/apache2/sites-available`" ] 
+if [ -z "`ls /etc/apache2/sites-available`" ] 
 then
 	cp -R /etc-start/apache2/sites-available/ /etc/apache2/sites-available
 fi
 
-if [ -z "`/etc/php5`" ] 
+if [ -z "`ls /etc/php5`" ] 
 then
 	cp -R /etc-start/php5/ /etc/php5
 fi
