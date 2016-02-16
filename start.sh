c6947cf6635b9c80b5969c1ab8129e1a62ac042e
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-02-16 18:34:39 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 export TERM=xterm
 
-if [ -z "`etc/apache2/sites-available`" ] 
+if [ -z "`/etc/apache2/sites-available`" ] 
 then
 	rsync -a /etc-start/apache2/sites-available/* /etc/apache2/sites-available
+fi
+
+if [ -z "`/etc/php/7.0`" ] 
+then
+	rsync -a /etc-start/php/7.0/* /etc/php/7.0
 fi
 
 # Start php7
