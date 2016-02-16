48042b278391fc8812916b0765b8678b52a9b170
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-02-17 01:29:20 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 export TERM=xterm
 
-if [ -z "`/etc/apache2/sites-available`" ] 
+if [ -z "`ls /etc/apache2/sites-available`" ] 
 then
 	cp -R /etc-start/apache2/sites-available/ /etc/apache2/sites-available
 fi
