cd3a704fa20d5f4c7316b09caff142f3e9c408e3
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-02-19 18:36:09 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ export TERM=xterm
 
 if [ -z "`ls /etc/apache2/sites-available`" ] 
 then
-	cp -R /etc-start/apache2/sites-available/ /etc/apache2/sites-available
+	cp -R /etc-start/apache2/sites-available/* /etc/apache2/sites-available
 fi
 
 if [ -z "`ls /etc/php/7.0`" ] 
