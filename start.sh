73f6a98537a8c100cdacf84ce63ff84e16677455
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-07-09 09:14:59 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,11 @@ then
 	cp -R /etc-start/apache2/sites-available/* /etc/apache2/sites-available
 fi
 
+if [ -z "`ls /etc/apache2/sites-enable`" ] 
+then
+	cp -R /etc-start/apache2/sites-enable/* /etc/apache2/sites-enable
+fi
+
 if [ -z "`ls /etc/php/7.0`" ] 
 then
 	cp -R /etc-start/php/7.0/* /etc/php/7.0
