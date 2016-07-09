4514219932c89ca42646fbfb452414e9cb83b58a
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-07-09 09:14:03 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,11 @@ then
 	cp -R /etc-start/apache2/sites-available/* /etc/apache2/sites-available
 fi
 
+if [ -z "`ls /etc/apache2/sites-enable`" ] 
+then
+	cp -R /etc-start/apache2/sites-enable/* /etc/apache2/sites-enable
+fi
+
 if [ -z "`ls /etc/php5`" ] 
 then
 	cp -R /etc-start/php5/* /etc/php5
