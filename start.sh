e490be66b8f39a53fab9330fdff1f020cbd8128d
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-07-09 09:13:46 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,5 +7,10 @@ then
 	cp -R /etc-start/apache2/sites-available/* /etc/apache2/sites-available
 fi
 
+if [ -z "`ls /etc/apache2/sites-enable`" ] 
+then
+	cp -R /etc-start/apache2/sites-enable/* /etc/apache2/sites-enable
+fi
+
 # Start apache
 /usr/sbin/apache2 -D FOREGROUND
