53f0080ae3a09f14ac0e1b3045c04b5e61f7c6c9
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-07 00:36:15 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 export TERM=xterm
 
-if [ -z "`ls /etc/apache2/sites-available`" ] 
+if [ -z "`ls /etc/apache2`" ] 
 then
-	cp -R /etc-start/apache2/sites-available/* /etc/apache2/sites-available
+	cp -R /etc-start/apache2/* /etc/apache2
 fi
 
-if [ -z "`ls /etc/apache2/sites-enabled`" ] 
+if [ -z "`ls /etc/php7`" ] 
 then
-	cp -R /etc-start/apache2/sites-enabled/* /etc/apache2/sites-enabled
-fi
-
-if [ -z "`ls /etc/php/7.0`" ] 
-then
-	cp -R /etc-start/php/7.0/* /etc/php/7.0
+	cp -R /etc-start/php7/* /etc/php7
 fi
 
 # Start php7
 #service php7.0 start
 # Start apache
-/usr/sbin/apache2 -D FOREGROUND
+exec /usr/sbin/httpd -D FOREGROUND
