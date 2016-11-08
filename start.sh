bec262e3dd744e1526d8ee46310c5f72d13a84e1
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-08 22:57:16 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,6 @@ if [ -z "`ls /etc/apache2/sites-available`" ]
 then
 	cp -R /etc-start/apache2/sites-available/* /etc/apache2/sites-available
 fi
-sleep 2
 if [ -z "`ls /etc/apache2/sites-enabled`" ] 
 then
 	cp -R /etc-start/apache2/sites-enabled/* /etc/apache2/sites-enabled
