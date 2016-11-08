d859163c1082fb5829a98bcd339ef0fbd56201a8
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-08 20:30:37 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 export TERM=xterm
 
 # Prepare
-if [ ! -z "`ls /etc/apache2`" ]; then cp -R /etc-start/apache2/* /etc/apache2; fi
-if [ ! -z "`ls /etc/php7`" ]; then cp -R /etc-start/php7/* /etc/php7; fi
-if [ ! -z "`ls /var/www`" ]; then cp -R /etc-start/www/* /var/www; fi
+if [ -z "`ls /etc/apache2`" ]; then cp -R /etc-start/apache2/* /etc/apache2; fi
+if [ -z "`ls /etc/php7`" ]; then cp -R /etc-start/php7/* /etc/php7; fi
+if [ -z "`ls /var/www`" ]; then cp -R /etc-start/www/* /var/www; fi
 
    # Set environments
     TIMEZONE1=${TIMEZONE:-Asia/Ho_Chi_Minh}
