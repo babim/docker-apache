92360bd26d12221dc2a5c61ccf0ed539273ee5ab
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-08 20:07:32 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,14 +2,9 @@
 
 export TERM=xterm
 
-if [ -z "`ls /var/www`" ] 
-then
-	cp -R /etc-start/www/* /var/www
-fi
-if [ -z "`ls /etc/apache2`" ] 
-then
-	cp -R /etc-start/apache2/* /etc/apache2
-fi
+# Prepare
+if [ ! -z "`ls /etc/apache2`" ]; then cp -R /etc-start/apache2/* /etc/apache2; fi
+if [ ! -z "`ls /var/www`" ]; then cp -R /etc-start/www/* /var/www; fi
 
 # Start apache
 exec httpd -D FOREGROUND
