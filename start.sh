d45a933c0e4ce2813b3581716d3f49e5d50b54e2
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-09 20:36:20 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 export TERM=xterm
 
-if [ -z "`ls /etc/apache2/sites-available`" ] 
-then
-	cp -R /etc-start/apache2/sites-available/* /etc/apache2/sites-available
-fi
+if [ -z "`ls /etc/apache2`" ]; then cp -R /etc-start/apache2/* /etc/apache2; fi
+if [ -z "`ls /etc/php/7.0`" ]; then cp -R /etc-start/php/7.0/* /etc/php/7.0; fi
 
-if [ -z "`ls /etc/apache2/sites-enabled`" ] 
-then
-	cp -R /etc-start/apache2/sites-enabled/* /etc/apache2/sites-enabled
-fi
+   # Set environments
+    TIMEZONE1=${TIMEZONE:-Asia/Ho_Chi_Minh}
+    PHP_MEMORY_LIMIT1=${PHP_MEMORY_LIMIT:-512M}
+    MAX_UPLOAD1=${MAX_UPLOAD:-520M}
+    PHP_MAX_FILE_UPLOAD1=${PHP_MAX_FILE_UPLOAD:-200}
+    PHP_MAX_POST1=${PHP_MAX_POST:-520M}
+    MAX_INPUT_TIME1=${MAX_INPUT_TIME:-3600}
+    MAX_EXECUTION_TIME1=${MAX_EXECUTION_TIME:-3600}
+	
+	sed -i -E \
+	-e "s|;*date.timezone =.*|date.timezone = ${TIMEZONE1}|i" \
+	-e "s|;*memory_limit =.*|memory_limit = ${MAX_UPLOAD1}|i" \
+ 	-e "s|;*upload_max_filesize =.*|upload_max_filesize = ${MAX_UPLOAD1}|i" \
+    	-e "s|;*max_file_uploads =.*|max_file_uploads = ${PHP_MAX_FILE_UPLOAD1}|i" \
+    	-e "s|;*post_max_size =.*|post_max_size = ${PHP_MAX_POST1}|i" \
+    	-e "s/max_input_time = 60/max_input_time = ${MAX_INPUT_TIME1}/" \
+	-e "s/max_execution_time = 30/max_execution_time = ${MAX_EXECUTION_TIME1}/" \
+	/etc/php/7.0/*/php.ini
 
-if [ -z "`ls /etc/php/7.0`" ] 
-then
-	cp -R /etc-start/php/7.0/* /etc/php/7.0
-fi
-
-# Start php7
-#service php7.0 start
 # Start apache
 apache2 -D FOREGROUND
