f46eed3b6077b1d600e6c82d3ddb83df74a224bc
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-12-11 03:18:13 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -34,15 +34,15 @@ elif [[ "$auid" = "0" ]] || [[ "$aguid" == "0" ]]; then
 	auser=root
 	export APACHE_RUN_USER=$auser
 	export APACHE_RUN_GROUP=$auser
-	sed -i -e "/^user = .*/cuser = $auser" /etc/php/5.6/fpm/php-fpm.conf
-	sed -i -e "/^group = .*/cgroup = $auser" /etc/php/5.6/fpm/php-fpm.conf
+	#sed -i -e "/^user = .*/cuser = $auser" /etc/php/5.6/fpm/php-fpm.conf
+	#sed -i -e "/^group = .*/cgroup = $auser" /etc/php/5.6/fpm/php-fpm.conf
 	#sed -i -e "/^user .*/cuser  $auser;" /etc/nginx/nginx.conf
 	#sed -i -e "/^#user .*/cuser  $auser;" /etc/nginx/nginx.conf
 else
 if id $auser >/dev/null 2>&1; then
         echo "user exists"
-	sed -i -e "/^user = .*/cuser = $auser" /etc/php/5.6/fpm/php-fpm.conf
-	sed -i -e "/^group = .*/cgroup = $auser" /etc/php/5.6/fpm/php-fpm.conf
+	#sed -i -e "/^user = .*/cuser = $auser" /etc/php/5.6/fpm/php-fpm.conf
+	#sed -i -e "/^group = .*/cgroup = $auser" /etc/php/5.6/fpm/php-fpm.conf
 	#sed -i -e "/^user .*/cuser  $auser;" /etc/nginx/nginx.conf
 	#sed -i -e "/^#user .*/cuser  $auser;" /etc/nginx/nginx.conf
 	export APACHE_RUN_USER=$auser
@@ -61,8 +61,8 @@ else
 	#addgroup -g $agid $auser && adduser -D -H -G $auser -s /bin/false -u $auid $auser
 	# create user ubuntu/debian
 	groupadd -g $agid $auser && useradd --system --uid $auid --shell /usr/sbin/nologin -g $auser $auser
-	sed -i -e "/^user = .*/cuser = $auser" /etc/php/5.6/fpm/php-fpm.conf
-	sed -i -e "/^group = .*/cgroup = $auser" /etc/php/5.6/fpm/php-fpm.conf
+	#sed -i -e "/^user = .*/cuser = $auser" /etc/php/5.6/fpm/php-fpm.conf
+	#sed -i -e "/^group = .*/cgroup = $auser" /etc/php/5.6/fpm/php-fpm.conf
 	#sed -i -e "/^user .*/cuser  $auser;" /etc/nginx/nginx.conf
 	#sed -i -e "/^#user .*/cuser  $auser;" /etc/nginx/nginx.conf
 fi
