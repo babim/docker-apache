eefb6175ea336cfa19ccc93f5c0663937444772e
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-12-11 03:16:25 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -34,15 +34,48 @@ if [ -z "`ls /var/www`" ]; then cp -R /etc-start/www/* /var/www; fi
 
 # set ID docker run
 agid=${agid:-$auid}
-auser=apache
+auser=${auser:-www-data}
 
 if [[ -z "${auid}" ]]; then
   echo "start"
 elif [[ "$auid" = "0" ]] || [[ "$aguid" == "0" ]]; then
- echo "can't run in Root user. Default user still run."
+	echo "run in user root"
+	auser=root
+	export APACHE_RUN_USER=$auser
+	export APACHE_RUN_GROUP=$auser
+	#sed -i -e "/^user = .*/cuser = $auser" /etc/php/7.0/fpm/php-fpm.conf
+	#sed -i -e "/^group = .*/cgroup = $auser" /etc/php/7.0/fpm/php-fpm.conf
+	#sed -i -e "/^user .*/cuser  $auser;" /etc/nginx/nginx.conf
+	#sed -i -e "/^#user .*/cuser  $auser;" /etc/nginx/nginx.conf
 else
-  usermod -u $auid $auser
-  groupmod -g $agid $auser
+if id $auser >/dev/null 2>&1; then
+        echo "user exists"
+	#sed -i -e "/^user = .*/cuser = $auser" /etc/php/7.0/fpm/php-fpm.conf
+	#sed -i -e "/^group = .*/cgroup = $auser" /etc/php/7.0/fpm/php-fpm.conf
+	#sed -i -e "/^user .*/cuser  $auser;" /etc/nginx/nginx.conf
+	#sed -i -e "/^#user .*/cuser  $auser;" /etc/nginx/nginx.conf
+	export APACHE_RUN_USER=$auser
+	export APACHE_RUN_GROUP=$auser
+	# usermod alpine
+		deluser $auser && delgroup $auser
+		addgroup -g $agid $auser && adduser -D -H -G $auser -s /bin/false -u $auid $auser
+	# usermod ubuntu/debian
+		#usermod -u $auid $auser
+		#groupmod -g $agid $auser
+else
+        echo "user does not exist"
+	export APACHE_RUN_USER=$auser
+	export APACHE_RUN_GROUP=$auser
+	# create user alpine
+	addgroup -g $agid $auser && adduser -D -H -G $auser -s /bin/false -u $auid $auser
+	# create user ubuntu/debian
+	#groupadd -g $agid $auser && useradd --system --uid $auid --shell /usr/sbin/nologin -g $auser $auser
+	#sed -i -e "/^user = .*/cuser = $auser" /etc/php/7.0/fpm/php-fpm.conf
+	#sed -i -e "/^group = .*/cgroup = $auser" /etc/php/7.0/fpm/php-fpm.conf
+	#sed -i -e "/^user .*/cuser  $auser;" /etc/nginx/nginx.conf
+	#sed -i -e "/^#user .*/cuser  $auser;" /etc/nginx/nginx.conf
+fi
+
 fi
 
 # Start apache
