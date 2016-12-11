8ebdb7e5aa40c9900cb0a691523e808800e6017f
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-12-11 12:34:51 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -21,6 +21,8 @@ elif [[ "$auid" = "0" ]] || [[ "$aguid" == "0" ]]; then
 	#sed -i -e "/^group = .*/cgroup = $auser" /etc/php/5.6/fpm/php-fpm.conf
 	#sed -i -e "/^user .*/cuser  $auser;" /etc/nginx/nginx.conf
 	#sed -i -e "/^#user .*/cuser  $auser;" /etc/nginx/nginx.conf
+elif id $auid >/dev/null 2>&1; then
+        echo "UID exists. Please change UID"
 else
 if id $auser >/dev/null 2>&1; then
         echo "user exists"
