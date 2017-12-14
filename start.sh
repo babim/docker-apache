d1cfc1f450108ede3d54d031b80db0f9456a6a82
Duc Anh Babim <ducanh.babim@yahoo.com>
2017-12-14 09:40:53 +0700

    123

--- start.sh
+++ start.sh
@@ -79,5 +79,12 @@ fi
 
 fi
 
+# ssh
+if [ -f "/runssh.sh" ]; then /runssh.sh; fi
+# cron
+if [ -f "/runcron.sh" ]; then /runcron.sh; fi
+# nfs
+if [ -f "/mountnfs.sh" ]; then /mountnfs.sh; fi
+
 # Start apache
 apache2 -D FOREGROUND
