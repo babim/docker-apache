a557c00b3a17b51f1d6241a15e7f181dff02e150
Duc Anh Babim <ducanh.babim@yahoo.com>
2018-06-28 17:45:08 +0700

    1

--- start.sh
+++ start.sh
@@ -14,24 +14,36 @@ if [ -z "`ls /etc/php`" ]; then
     MAX_INPUT_TIME1=${MAX_INPUT_TIME:-3600}
     MAX_EXECUTION_TIME1=${MAX_EXECUTION_TIME:-3600}
 	
+	# set php value
+	for VARIABLE in /etc/php/*
+	do
 	sed -i -E \
 	-e "s|;*date.timezone =.*|date.timezone = ${TIMEZONE1}|i" \
 	-e "s|;*memory_limit =.*|memory_limit = ${MAX_UPLOAD1}|i" \
  	-e "s|;*upload_max_filesize =.*|upload_max_filesize = ${MAX_UPLOAD1}|i" \
-    	-e "s|;*max_file_uploads =.*|max_file_uploads = ${PHP_MAX_FILE_UPLOAD1}|i" \
-    	-e "s|;*post_max_size =.*|post_max_size = ${PHP_MAX_POST1}|i" \
-    	-e "s/max_input_time = 60/max_input_time = ${MAX_INPUT_TIME1}/" \
+    -e "s|;*max_file_uploads =.*|max_file_uploads = ${PHP_MAX_FILE_UPLOAD1}|i" \
+    -e "s|;*post_max_size =.*|post_max_size = ${PHP_MAX_POST1}|i" \
+    -e "s/max_input_time = 60/max_input_time = ${MAX_INPUT_TIME1}/" \
 	-e "s/max_execution_time = 30/max_execution_time = ${MAX_EXECUTION_TIME1}/" \
 	-e "s/error_reporting = .*/error_reporting = E_ALL/" \
 	-e "s/display_errors = .*/display_errors = On/" \
-	/etc/php/7.0/*/php.ini
-	sed -i -e "s|^;*\(opcache.enable\) *=.*|\1 = 1|" /etc/php/7.0/*/php.ini
-        sed -i -e "s|^;*\(opcache.enable_cli\) *=.*|\1 = 1|" /etc/php/7.0/*/php.ini
-        sed -i -e "s|^;*\(opcache.fast_shutdown\) *=.*|\1 = 1|" /etc/php/7.0/*/php.ini
-        sed -i -e "s|^;*\(opcache.interned_strings_buffer\) *=.*|\1 = 8|" /etc/php/7.0/*/php.ini
-        sed -i -e "s|^;*\(opcache.max_accelerated_files\) *=.*|\1 = 4000|" /etc/php/7.0/*/php.ini
-        sed -i -e "s|^;*\(opcache.memory_consumption\) *=.*|\1 = 128|" /etc/php/7.0/*/php.ini
-	sed -i -e "s|^;*\(opcache.revalidate_freq\) *=.*|\1 = 60|" /etc/php/7.0/*/php.ini
+	$VARIABLE/php.ini
+	done
+
+	# set opcache
+	for VARIABLE in /etc/php/*
+	do
+	sed -i -E \
+	-e "s|^;*\(opcache.enable\) *=.*|\1 = 1|" \
+	-e "s|^;*\(opcache.enable_cli\) *=.*|\1 = 1|" \
+ 	-e "s|^;*\(opcache.fast_shutdown\) *=.*|\1 = 1|" \
+    -e "s|^;*\(opcache.interned_strings_buffer\) *=.*|\1 = 8|" \
+    -e "s|^;*\(opcache.max_accelerated_files\) *=.*|\1 = 4000|" \
+    -e "s|^;*\(opcache.memory_consumption\) *=.*|\1 = 128|" \
+	-e "s|^;*\(opcache.revalidate_freq\) *=.*|\1 = 60|" \
+	$VARIABLE/php.ini
+	done
+
 fi
 if [ -z "`ls /var/www`" ]; then
 	cp -R /etc-start/www/* /var/www
